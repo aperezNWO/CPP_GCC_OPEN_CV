42c31d33220c2b522b299c56f79df552f5642e47
aperezNWO <alejandro.perez.acosta@hotmail.com>
2025-05-01 21:30:40 -0500

    Application CPP_GCC_OPEN_CV v[1.0.1]
    1) Version inicial estable

---
+++ mingwin/build/CMakeFiles/OpenCvDll.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Users/pablo.perez/dev/cpp/CPP_GCC_OPEN_CV/mingwin/bin/OpenCvDll.dll"
+  "C:/Users/pablo.perez/dev/cpp/CPP_GCC_OPEN_CV/mingwin/bin/OpenCvDll.dll.a"
+  "C:/Users/pablo.perez/dev/cpp/CPP_GCC_OPEN_CV/mingwin/bin/OpenCvDll.dll.manifest"
+  "C:/Users/pablo.perez/dev/cpp/CPP_GCC_OPEN_CV/mingwin/bin/OpenCvDll.pdb"
+  "CMakeFiles/OpenCvDll.dir/OpenCvDll.cpp.obj"
+  "CMakeFiles/OpenCvDll.dir/OpenCvDll.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCvDll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
